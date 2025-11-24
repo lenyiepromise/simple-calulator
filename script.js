8fcbff7b344de5334082c641c6fe63e733374d20
lp.barida <lenyiepromise@gmail.com>
2025-11-24 10:09:53 +0100

    Add comment

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
+// Display screen
 const display = document.getElementById('display');
 
+// Number buttons
 function appendToDisplay(value) {
 	display.value += value;
 }
 
+// Operation buttons
 function backspace() {
 	display.value = display.value.slice(0, -1);
 }
 
+// Clear button
 function clearDisplay() {
 	display.value = '';
 }
 
+// Equals button
 function calculate() {
 	try {
 		display.value = eval(display.value);
